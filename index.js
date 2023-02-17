ba2585a4fbb985339f253dbb585d326bc645da03
coder723237 <121548799+coder723237@users.noreply.github.com>
2023-02-18 02:15:08 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const daysEl = document.querySelector(".days");
 const monthInx = new Date().getMonth();
 const lastDay = new Date(new Date().getFullYear(), monthInx + 1, 0).getDate();
 const firstDay = new Date(new Date().getFullYear(), monthInx, 1).getDay() - 1;
-console.log(firstDay);
 
 const months = [
   "January",
@@ -27,16 +26,15 @@ fullDateEl.innerText = new Date().toDateString();
 
 let days = "";
 
-for (let i = firstDay; i>0; i--){
-  days += `<div class="empty"></div>`;
+for (let i = firstDay; i > 0; i--) { 
+  days += `<div class="empty"></div>`; 
 }
-
-for (let i = 1; i <= lastDay; i++){
-  if( i = new Date().getDate()){
+for (let i = 1; i <= lastDay; i++) {
+  if( i === new Date().getDate()) {
     days += `<div class="today">${i}</div>`;
   } else {
-  days += `<div>${i}</div>`;
+    days += `<div>${i}</div>`;
   }
-}
+} 
 
 daysEl.innerHTML = days;
